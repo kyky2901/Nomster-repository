0fcbc7883ad72ffd34526da29bd2464b3b2a5d42
Kylie <kylie.rae.slattery@gmail.com>
2019-04-09 04:50:42 +0000

    Tried to fix error message

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,21 +1,22 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-def index
-  @places = Place.all
- end  
-end
 
-def new 
-  @place = Place.new
-end
+  def index
+    @places = Place.all
+  end
 
-def create
-  current_user.places.create(place_params)
-  redirect_to root_path
-end
+  def new
+    @place = Place.new
+  end
 
-private 
+  def create
+    current_user.places.create(place_params)
+    redirect_to root_path
+  end
 
-def place_params
-  params.require(:place).permit(:name, :description, :address)
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
