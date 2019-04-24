13f8e98895c743e1a58d531e33869e5fdcfe0c88
Kylie <kylie.rae.slattery@gmail.com>
2019-04-24 02:06:07 +0000

    Previewing the email - still does nnot work

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "kylie.rae.slattery@gmail.com",
-      subject: "A comment has been added to your place")
+    mail(to: "kslat71@gmail.com",
+         subject: "A comment has been added to your place")
   end
 end
